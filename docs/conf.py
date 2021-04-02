6700b35fe9554f9d34447b3d9852d820ffb13054
Casper da Costa-Luis <casper.dcl@physics.org>
2021-04-02 16:39:38 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, Casper da Costa-Luis'
 author = 'Casper da Costa-Luis'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.0'
+release = 'v1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
